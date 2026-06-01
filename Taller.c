510a2f71e339237e8214e0eeff33d2f85924be01
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-06-01 10:27:28 -0500

    Sexto commit cierre y verificacion que el codigo compile correctamente

--- Taller.c
+++ Taller.c
@@ -25,7 +25,7 @@ float notas[n][3];
         }
     }
     
-    printf("\n=== RESULTADOS POR ESTUDIANTE ===\n");
+  printf("\n=== RESULTADOS POR ESTUDIANTE ===\n");
     for (int i = 0; i < n; i++) {
         float suma = 0, max = notas[i][0], min = notas[i][0];
         for (int j = 0; j < 3; j++) {
@@ -34,5 +34,23 @@ float notas[n][3];
             if (notas[i][j] < min) min = notas[i][j];
         }
         printf("Estudiante %d -> Promedio: %.2f | Max: %.2f | Min: %.2f\n", 
-               i + 1, suma / n, max, min); 
+               i + 1, suma / 3.0, max, min);
     }
+    printf("\n=== RESULTADOS POR ASIGNATURA ===\n");
+    for (int j = 0; j < 3; j++) {
+        float suma = 0, max = notas[0][j], min = notas[0][j];
+        int aprobados = 0, reprobados = 0;
+        
+        for (int i = 0; i < n; i++) {
+            suma += notas[i][j];
+            if (notas[i][j] > max) max = notas[i][j];
+            if (notas[i][j] < min) min = notas[i][j];
+            
+            if (notas[i][j] >= 6.0) aprobados++;
+            else reprobados++;
+        }
+        printf("Asignatura %d -> Promedio: %.2f | Max: %.2f | Min: %.2f | Aprobados: %d | Reprobados: %d\n", 
+               j + 1, suma / n, max, min, aprobados, reprobados);
+            }
+            return 0;
+}
